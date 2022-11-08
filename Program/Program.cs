056c7f8a494452212f002b764016e34ac45e1a58
Tom Gillette <tomwgillette@gmail.com>
2022-11-08 16:46:56 -0500

    FirstCommit

---
+++ Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ProbpracticeGen
+{
+    public class Practice
+    {
+        public string Name { get; set; }
+        public List<string> Problem { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "Practice.json";
+            string jsonString = File.ReadAllText(fileName);
+            List<Practice> Practices = JsonSerializer.Deserialize<List<Practice>>(jsonString)!;
+            Console.WriteLine(Practices.Count);
+        }
+    }
+}
